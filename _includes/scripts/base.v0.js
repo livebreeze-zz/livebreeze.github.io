388461753fdf552b9b135254e294f1788670d347
livebreeze <livebreeze@gmail.com>
2017-02-07 22:57:19 +0800

    update debug info

--- _includes/scripts/base.v0.js
+++ _includes/scripts/base.v0.js
@@ -2,9 +2,8 @@
 // debug alert
 (function() {
     if (console && typeof (console.log) === "function") {
-        console.log('%cHi! 力賀!', 'color:lightblue');
+        console.log('%cHi!', 'color:lightblue');
         console.log('Any suggestion, please let me kwnow, %cthank you!', 'color:orange');
-        console.log('本魯文章有任何不妥，請讓我知道，%c港謝您~', 'color:orange');
         console.log('%clivebreeze@gmail.com', 'color: cadetblue');
     }
 })();
